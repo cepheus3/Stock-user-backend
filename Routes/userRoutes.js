61ffd61d8b438e24bd933bfc541bc076068575b9
cepheus3 <nebiyouelias96@gmail.com>
2023-05-30 14:04:12 -0400

    get all user list done

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -8,10 +8,12 @@ const {
 const { signup } = require('../Controller/user/Signup');
 const { Signin } = require('../Controller/user/Signin');
 const { verifyOtp } = require('../Controller/VerifyOtp');
+const { getAllUser} = require('../Controller/Admin/GetAllUser')
 /* const {s} = require("../Controller/user/Signin")
  */
 router.post('/user/signup', validateRequest, isValidatedRequest, signup);
 router.post('/user/verifyotp', verifyOtp);
 router.post('/user/signin',validatesigninRequest,isValidatedRequest,Signin);
+router.get('/getalluser',getAllUser);
 
 module.exports = router;
